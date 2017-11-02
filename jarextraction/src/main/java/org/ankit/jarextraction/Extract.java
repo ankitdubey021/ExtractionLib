c6b1517b613513f846a95b8fd775a62c5f697ef6
Ankit Dubey <ankitdubey021@gmail.com>
2017-11-02 17:14:07 +0530

    Added extract file

--- jarextraction/src/main/java/org/ankit/jarextraction/Extract.java
+++ jarextraction/src/main/java/org/ankit/jarextraction/Extract.java
@@ -4,7 +4,7 @@ package org.ankit.jarextraction;
  * Created by Administrator on 11/2/2017.
  */
 
-public class Extract {
+public class Extract{
     public static void extractJar(){
         System.out.println("extracting......................");
     }
