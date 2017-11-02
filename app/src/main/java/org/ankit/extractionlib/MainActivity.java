3a9f5ca5ac913b329699daa7102e8b4005a5ef04
Ankit Dubey <ankitdubey021@gmail.com>
2017-11-02 18:00:59 +0530

    done

--- app/src/main/java/org/ankit/extractionlib/MainActivity.java
+++ app/src/main/java/org/ankit/extractionlib/MainActivity.java
@@ -1,9 +1,18 @@
 package org.ankit.extractionlib;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import org.ankit.jarextraction.Extract;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +20,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Extract.extractJar();
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                        extract();
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+        }
+    }
+    void extract(){
+        try {
+
+            File outputFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "value1.jar");
+
+            Extract.extract(outputFile, new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "extration").toString());
+            Toast.makeText(MainActivity.this, "Extracted!", Toast.LENGTH_SHORT).show();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
--- jarextraction/src/main/java/org/ankit/jarextraction/Extract.java
+++ jarextraction/src/main/java/org/ankit/jarextraction/Extract.java
@@ -1,11 +1,45 @@
 package org.ankit.jarextraction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.io.File;
+
 /**
  * Created by Administrator on 11/2/2017.
  */
 
-public class Extract{
-    public static void extractJar(){
-        System.out.println("extracting......................");
+public class Extract extends ActivityCompat{
+    public static void extract(File jarfile, String path)throws Exception{
+
+
+        java.util.jar.JarFile jar = new java.util.jar.JarFile(jarfile);
+        java.util.Enumeration enums = jar.entries();
+
+        while (enums.hasMoreElements()) {
+            java.util.jar.JarEntry file = (java.util.jar.JarEntry) enums.nextElement();
+            java.io.File myFile=new java.io.File(path);
+            if(!myFile.exists())
+                myFile.mkdir();
+
+            java.io.File f = new java.io.File(myFile + java.io.File.separator + file.getName());
+            if (file.isDirectory()) { // if its a directory, create it
+                f.mkdir();
+                continue;
+            }
+
+            java.io.InputStream is = jar.getInputStream(file); // get the input stream
+            java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
+            while (is.available() > 0) {  // write contents of 'is' to 'fos'
+                fos.write(is.read());
+            }
+
+            fos.close();
+            is.close();
+        }
     }
+
+
 }
